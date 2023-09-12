dc09ac12635c61b69d773106238d38753ea012d3
­곽유신 <joke79@hanyang.ac.kr>
2023-09-12 13:54:05 +0900

    update users pagination

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@root/users/entities/user.entity';
+import { PageOptionsDto } from '@root/common/dtos/page-options.dto';
+import { PageDto } from '@root/common/dtos/page.dto';
+import { Question } from '@questions/entities/question.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async getUserList(): Promise<User[]> {
-    return await this.usersService.getUserList();
+  async getUserList(
+    @Query() pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<User>> {
+    return await this.usersService.getUserList(pageOptionsDto);
   }
 
   @Post('email')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '@root/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '@root/users/dto/create-user.dto';
+import { PageOptionsDto } from '@root/common/dtos/page-options.dto';
+import { PageMetaDto } from '@root/common/dtos/page-meta.dto';
+import { PageDto } from '@root/common/dtos/page.dto';
+import { Question } from '@questions/entities/question.entity';
 
 @Injectable()
 export class UsersService {
@@ -11,9 +15,20 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async getUserList(): Promise<User[]> {
-    const users = await this.usersRepository.find();
-    return users;
+  async getUserList(pageOptionsDto: PageOptionsDto): Promise<PageDto<User>> {
+    // const users = await this.usersRepository.find();
+    // return users;
+
+    const queryBuilder = await this.usersRepository.createQueryBuilder('users');
+    queryBuilder
+      .orderBy('users.createdAt', pageOptionsDto.order)
+      .skip(pageOptionsDto.skip)
+      .take(pageOptionsDto.take);
+    const itemCount = await queryBuilder.getCount();
+    const { entities } = await queryBuilder.getRawAndEntities();
+
+    const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
+    return new PageDto(entities, pageMetaDto);
   }
 
   async getUserById(id: string): Promise<User> {
